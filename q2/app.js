e22f94a7b817080d725b93ad9cf8e44983a4d159
pasha <mtipasha2021@gmail.com>
2024-10-04 16:26:44 +0330

    new

---
+++ q2/app.js
@@ -0,0 +1,13 @@
+// const array = [
+//   { name: "banana", count: 2, pricePerOne: 45000, discount: 5 },
+//   { name: "potato", count: 8, pricePerOne: 23000, discount: 15 },
+//   { name: "onion", count: 12, pricePerOne: 12000, discount: 10 },
+// ];
+// for (const element of array) {
+//   let total =
+//     (element.count * element.pricePerOne * (100 - element.discount)) / 100;
+//   console.log(total);
+//   element.total = total;
+//   let per = (element.pricePerOne * (100 - element.discount)) / 100;
+// }
+// console.table(array);
